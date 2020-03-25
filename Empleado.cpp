c021bc37c7d2f183ffa52f90c7651ee25a492a05
MarioVelasquezN <41805686+MarioVelasquezN@users.noreply.github.com>
2020-03-24 23:45:11 -0600

    Update Empleado.cpp

--- Empleado.cpp
+++ Empleado.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 
 Empleado::Empleado() {
+	cedula[0] = 0;
 	nombre[0] = 0;
 	Apellido[0] = 0;
 	direccion[0] = 0;
@@ -16,7 +17,8 @@ Empleado::Empleado() {
 }
 
 void Empleado::Print() {
-	cout << nombre << " "
+	cout <<cedula<<" "
+		<< nombre << " "
 		<< Apellido << " "
 		<< direccion << " "
 		<< ciudad << " "
@@ -39,7 +41,21 @@ void Empleado::unPack() {
 	size_t j=0;
 	int pos = 0;
 
-	for (size_t i = 0; c[i]!=se ; i++)
+	for (size_t i = 0; c[i] != se; i++)
+	{
+		temp[j] = c[i];
+
+		j++;
+		pos++;
+	}
+	temp[j] = '\0';
+
+	strcpy(cedula, temp);
+
+	j = 0;
+	pos = pos + 1;
+
+	for (size_t i = pos; c[i]!=se ; i++)
 	{
 		temp[j] = c[i];
 		
@@ -106,82 +122,38 @@ void Empleado::unPack() {
 	strcpy(zipCode, temp);
 
 
-	/*strcpy(buffer, nombre);
-	strcpy(buffer, &se);
-
-	strcpy(buffer, Apellido);
-	strcpy(buffer, &se);
-
-	strcpy(buffer, direccion);
-	strcpy(buffer, &se);
-
-	strcpy(buffer, ciudad);
-	strcpy(buffer, &se);
-
-	strcpy(buffer, estado);
-	strcpy(buffer, &se);
-
-	strcpy(buffer, zipCode);
-	strcpy(buffer, &se);*/
-
-	////memcpy(&se, c += 1, 1);
-	//memcpy(nombre, c, 1);
-	//nombre[size] = 0;
-	//memcpy(&se, c += 1, 1);
-
-	//memcpy(Apellido, c+=1, 1);
-	//Apellido[size] = 0;
-	//memcpy(&se, c += 1, 1);
-
-	//memcpy(direccion, c += 1, 1);
-	//direccion[size] = 0;
-	//memcpy(&se, c += 1, 1);
-
-	//memcpy(ciudad, c += 1, 1);
-	//ciudad[size] = 0;
-	//memcpy(&se, c += 1, 1);
-
-	//memcpy(estado, c += 1, 1);
-	//estado[size] = 0;
-	//memcpy(&se, c += 1, 1);
-
-	//memcpy(zipCode, c += 1, 1);
-	//zipCode[size] = 0;
-	//memcpy(&se, c += 1, 1);
+	
 }
 
 void Empleado::Pack() {
-	//char str_size[3];
-
-	//int l_size = 0;
-	//str_size[2] = '\0';
-
-	//l_size = strlen(nombre);
 	
+	strcat(in_buffer, cedula);
+	strcat(in_buffer, "|");
+
 	strcat(in_buffer, nombre);
 	strcat(in_buffer, "|");
 
-	//l_size = strlen(Apellido);
+	
 	
 	strcat(in_buffer, Apellido);
 	strcat(in_buffer, "|");
 
-	//l_size = strlen(direccion);
+
 	
 	strcat(in_buffer, direccion);
 	strcat(in_buffer, "|");
 
-	//l_size = strlen(ciudad);
+	
 	
 	strcat(in_buffer, ciudad);
 	strcat(in_buffer, "|");
 
-	//l_size = strlen(estado);
+	
 	
 	strcat(in_buffer, estado);
 	strcat(in_buffer, "|");
 
-	//l_size = strlen(zipCode);
+
 	
 	strcat(in_buffer, zipCode);
 	strcat(in_buffer, "|");
